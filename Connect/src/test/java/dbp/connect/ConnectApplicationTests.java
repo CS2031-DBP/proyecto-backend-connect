bcf077caa9ae218acbe918087978ad929c2fe637
zamir5895 <lmr293141@gmail.com>
2024-05-05 15:55:49 -0500

    creacion del spring initializer

---
+++ Connect/src/test/java/dbp/connect/ConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package dbp.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
