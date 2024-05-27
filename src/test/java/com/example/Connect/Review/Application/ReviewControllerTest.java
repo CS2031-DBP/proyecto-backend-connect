e9331b74e05ad7dc719de2b4a00273c09fe55d43
zamir5895 <lmr293141@gmail.com>
2024-05-26 22:07:48 -0500

    mas cambios

---
+++ src/test/java/com/example/Connect/Review/Application/ReviewControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.Connect.Review.Application;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReviewControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createReview() {
+    }
+
+    @Test
+    void getReview() {
+    }
+
+    @Test
+    void getReviewsByPublicacion() {
+    }
+
+    @Test
+    void getReviewsByUser() {
+    }
+
+    @Test
+    void deleteReview() {
+    }
+
+    @Test
+    void updateReview() {
+    }
+}
