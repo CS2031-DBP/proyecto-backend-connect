b7e7c7f310265ef014c64bedec9ce6db479d8bd1
zamir5895 <lmr293141@gmail.com>
2024-06-19 00:34:46 -0500

    configurando

---
+++ Connect/src/main/java/dbp/connect/Configuracion/S3Configuracion.java
@@ -0,0 +1,27 @@
+package dbp.connect.Configuracion;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Configuracion {
+    @Value("${cloiud.aws.credentials.access-Key}")
+    private String accessKey;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+    @Value("${cloud.aws.region.static}")
+    private String region;
+    @Bean
+    public AmazonS3 s3Client(){
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey,secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .withRegion(region)
+                .build();
+    }
+}
