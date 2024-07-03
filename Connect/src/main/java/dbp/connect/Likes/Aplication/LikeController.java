bf3a50492502cfa950b303a0921b0d0dc8b598e6
zamir5895 <lmr293141@gmail.com>
2024-07-03 10:38:02 -0500

    Implementando Like

--- Connect/src/main/java/dbp/connect/Likes/Aplication/LikeController.java
+++ Connect/src/main/java/dbp/connect/Likes/Aplication/LikeController.java
@@ -1,10 +1,14 @@
 package dbp.connect.Likes.Aplication;
 
+import dbp.connect.Likes.DTOS.LikeResponseDTO;
+import dbp.connect.Likes.DTOS.LikesOfUserDTO;
 import dbp.connect.Likes.Domain.Like;
 import dbp.connect.Likes.Domain.LikeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/likes")
@@ -18,27 +22,36 @@ public class LikeController {
         return ResponseEntity.ok().build();
     }
     @GetMapping("/{likeId}")
-    public ResponseEntity<Like> getLikeById(@PathVariable Long likeId) {
-        return likeService.findLikeById(likeId)
-                .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+    public ResponseEntity<LikeResponseDTO> getLikeById(@PathVariable Long likeId) {
+        try{
+            LikeResponseDTO like = likeService.findLikeById(likeId);
+            return ResponseEntity.ok(like);}
+        catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping("/{likeId}")
     public ResponseEntity<Void> deleteLike(@PathVariable Long likeId) {
-        likeService.deleteLikeById(likeId);
+        likeService.deleteLikeByIdAsync(likeId);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/publicacion/{publicacionInicioId}")
-    public ResponseEntity<List<Like>> getLikesByPublicacion(@PathVariable Long publicacionInicioId) {
-        List<Like> likes = likeService.findLikesByPublicacionInicioId(publicacionInicioId);
+    public ResponseEntity<List<LikeResponseDTO>> getLikesByPublicacion(@PathVariable Long publicacionInicioId) {
+        List<LikeResponseDTO> likes = likeService.findLikesByPublicacionInicioId(publicacionInicioId);
         return ResponseEntity.ok(likes);
     }
 
     @GetMapping("/usuario/{usuarioLikeId}")
-    public ResponseEntity<List<Like>> getLikesByUsuario(@PathVariable Long usuarioLikeId) {
-        List<Like> likes = likeService.findLikesByUsuarioLikeId(usuarioLikeId);
+    public ResponseEntity<List<LikesOfUserDTO>> getLikesByUsuario(@PathVariable Long usuarioLikeId) {
+        List<LikesOfUserDTO> likes = likeService.findLikesByUsuarioLikeId(usuarioLikeId);
         return ResponseEntity.ok(likes);
     }
+    @GetMapping("{publicacionInicioId}/cantidad")
+    public ResponseEntity<Integer> getLikesCountByPublicacion(@PathVariable Long publicacionInicioId) {
+        Integer likes = likeService.findLikesCountByPublicacionInicioId(publicacionInicioId);
+        return ResponseEntity.ok(likes);
+    }
+
 }
--- Connect/src/main/java/dbp/connect/Likes/Domain/LikeService.java
+++ Connect/src/main/java/dbp/connect/Likes/Domain/LikeService.java
@@ -1,5 +1,7 @@
 package dbp.connect.Likes.Domain;
 
+import dbp.connect.Likes.DTOS.LikeResponseDTO;
+import dbp.connect.Likes.DTOS.LikesOfUserDTO;
 import dbp.connect.Likes.Infrastructure.LikeRepositorio;
 import dbp.connect.PublicacionInicio.Domain.PublicacionInicio;
 import dbp.connect.PublicacionInicio.Infrastructure.PublicacionInicioRepositorio;
@@ -11,6 +13,8 @@ import org.springframework.stereotype.Service;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -35,8 +39,72 @@ public class LikeService {
         like.setUsuarioLike(userLike);
         like.setFechaLike(ZonedDateTime.now(ZoneId.systemDefault()));
         likeRepositorio.save(like);
+        publicacionInicio.getLikes().add(like);
+        publicacionInicioRepositorio.save(publicacionInicio);
         System.out.println("Procesando like asincrono para publicacionInicioId: " + publicacionInicioId + " y usuarioLikeId: " + usuarioLikeId);
 
+    }
+    public LikeResponseDTO findLikeById(Long likeId) {
+        Like like = likeRepositorio.findById(likeId).orElseThrow(()
+                -> new IllegalArgumentException("Like not found"));
+        LikeResponseDTO likeResponseDTO = new LikeResponseDTO();
+        likeResponseDTO.setId(like.getId());
+        likeResponseDTO.setPublicacionInicioId(like.getPublicacionInicio().getId());
+        likeResponseDTO.setFechaLike(like.getFechaLike());
+        likeResponseDTO.setUsuarioLikeId(like.getUsuarioLike().getId());
+        likeResponseDTO.setUsuarioLikeUsername(like.getUsuarioLike().getUsername());
+        likeResponseDTO.setUsuarioFotoPerfil(like.getUsuarioLike().getFotoUrl());
+        return likeResponseDTO;
+    }
+    @Async
+    public void deleteLikeByIdAsync(Long likeId) {
+        Like like = likeRepositorio.findById(likeId).orElseThrow(()
+                -> new IllegalArgumentException("Like not found"));
+        PublicacionInicio publicacionInicio = like.getPublicacionInicio();
+        publicacionInicio.setCantidadLikes(publicacionInicio.getCantidadLikes() - 1);
+        likeRepositorio.deleteById(likeId);
+        publicacionInicio.getLikes().remove(like);
+        publicacionInicioRepositorio.save(publicacionInicio);
+
+        System.out.println("Procesando like asincrono para likeId: " + likeId);
+    }
+    public List<LikeResponseDTO> findLikesByPublicacionInicioId(Long publicacionInicioId) {
+        PublicacionInicio publicacionInicio = publicacionInicioRepositorio.findById(publicacionInicioId)
+                .orElseThrow(() -> new IllegalArgumentException("PublicacionInicio not found"));
+
+        return publicacionInicio.getLikes().stream()
+                .map(like -> {
+                    LikeResponseDTO dto = new LikeResponseDTO();
+                    dto.setId(like.getId());
+                    dto.setFechaLike(like.getFechaLike());
+                    dto.setPublicacionInicioId(like.getPublicacionInicio().getId());
+                    dto.setUsuarioLikeId(like.getUsuarioLike().getId());
+                    dto.setUsuarioLikeUsername(like.getUsuarioLike().getUsername());
+                    dto.setUsuarioFotoPerfil(like.getUsuarioLike().getFotoUrl());
+                    return dto;
+                })
+                .collect(Collectors.toList());
+    }
+    public List<LikesOfUserDTO> findLikesByUsuarioLikeId(Long usuarioLikeId) {
+        User user = userRepository.findById(usuarioLikeId).orElseThrow(()
+                -> new IllegalArgumentException("User not found"));
+
+        return user.getLikes().stream()
+                .map(like -> {
+                    LikesOfUserDTO dto = new LikesOfUserDTO();
+                    dto.setLikeId(like.getId());
+                    dto.setFechaLike(like.getFechaLike());
+                    dto.setPublicacionInicioId(like.getPublicacionInicio().getId());
+                    dto.setDescripcion(like.getPublicacionInicio().getCuerpo());
+                    return dto;
+                })
+                .collect(Collectors.toList());
+    }
+    public Integer findLikesCountByPublicacionInicioId(Long publicacionInicioId) {
+        PublicacionInicio publicacionInicio = publicacionInicioRepositorio.findById(publicacionInicioId)
+                .orElseThrow(() -> new IllegalArgumentException("PublicacionInicio not found"));
+        Integer likes = publicacionInicio.getLikes().size();
+        return likes;
     }
 
 }
